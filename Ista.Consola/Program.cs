d1e7e7ff9371d4b0dfabd4f51b79d4c070328a78
Javier <jmartin@grupoloyal.com>
2024-05-20 13:07:35 +0200

    consola

---
+++ Ista.Consola/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ista.Consola {
+    class Persona {
+        private string nombre;
+
+        public int Id { get; set; }
+        public string Nombre { 
+            get { 
+                return nombre?.ToLower(); 
+            } 
+            set { 
+                if(nombre == value) return;
+                nombre = value; 
+            } 
+        }
+        public bool EsConflictivo { get; }
+    }
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+            var p = new Persona();
+            p.Nombre = "algo";
+            Console.WriteLine(p.Nombre);
+        }
+    }
+}
