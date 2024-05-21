6f893957dfa371de05fc15f11aba2d71357b15e5
Javier <jmartin@grupoloyal.com>
2024-05-21 13:09:11 +0200

    Tipos

--- Ista.Comunes/Class1.cs
+++ Ista.Comunes/Class1.cs
@@ -1,5 +1,13 @@
 ﻿namespace Ista.Comunes {
+    /// <summary>
+    /// Ejemplos del curso
+    /// </summary>
     public class Demo {
+        /// <summary>
+        /// Bien educado que saluda
+        /// </summary>
+        /// <param name="nombre">El nombre a quien saluda,  por defecto a "Mundo"</param>
+        /// <returns>La cadena con el saludo</returns>
         public String Saluda(String nombre = "Mundo") {
             return $"Hola {nombre}";
         }
--- Ista.Consola/Program.cs
+++ Ista.Consola/Program.cs
@@ -2,39 +2,52 @@
 
 namespace Ista.Consola {
     public abstract class Persona {
+        #region Atributos
         private string nombre;
-
+        #endregion
+        #region Propiedades
         public int Id { get; set; }
-        public string Nombre { 
-            get { 
-                return nombre?.ToLower(); 
-            } 
-            set { 
+        public string Nombre {
+            get {
+                return nombre?.ToLower();
+            }
+            set {
                 if(nombre == value) return;
-                nombre = value; 
-            } 
+                nombre = value;
+            }
         }
         public bool EsConflictivo { get; }
+        #endregion
+        #region Métodos
 
         public virtual void Dime() {
             Console.WriteLine("Soy el padre");
-            
+
         }
         public void Add() {
             Console.WriteLine("Propio del Padre");
         }
 
         public abstract void DebeSobreescribir();
-
+        #endregion
     }
 
-    public class Alumno: Persona {
+#if DEBUG
+    public
+#else
+    private
+#endif
+    class Alumno : Persona {
         public override void Dime() {
             Console.WriteLine("Soy el Hijo");
         }
         public void Add() {
             Console.WriteLine("Propio del Hijo");
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="algo"></param>
         public void Add(String algo) {
             Console.WriteLine($"Propio del {algo}");
         }
@@ -43,18 +56,40 @@ namespace Ista.Consola {
             throw new NotImplementedException();
         }
     }
-    internal class Program {
+    public class App {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+#if MODO
+            Console.WriteLine("""Hello, World!""");
+#endif
+
             Persona p = new Alumno();
             p.Nombre = "algo";
+#if DEBUG
             Console.WriteLine(p.Nombre);
+#else
+            Console.WriteLine("p.Nombre");
+#endif
+#pragma warning disable CS8602 // Desreferencia de una referencia posiblemente NULL.
             (p as Alumno).Add();
+#pragma warning restore CS8602 // Desreferencia de una referencia posiblemente NULL.
             p.Dime();
             Console.WriteLine(p.Nombre);
             //p.Add();
             Demo demo = new Demo();
-            Console.WriteLine(demo.Saluda());
+            Console.WriteLine(demo.Saluda(""));
+            Console.WriteLine(0.1M+(decimal)0.2);
+            Console.WriteLine(1m-0.9m == 0.1m);
+            int div = 0;
+            Console.WriteLine((1 / div) * 0);
+            Object o = 4; // new Int32(4)
+            int? i = (int)o; // o.value()
+            i = null;
+            if(i.HasValue) {
+                Nullable<int> j = i.Value + 1;
+            }
+
+            long l = 99999999999;
+            int kk =(int) l;
         }
     }
 }
