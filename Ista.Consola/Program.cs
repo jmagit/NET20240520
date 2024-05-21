d6d336ea99aed3d54b720e39d5302ee954e5f2a4
Javier <jmartin@grupoloyal.com>
2024-05-21 10:51:28 +0200

    ejemplos

--- Ista.Consola/Program.cs
+++ Ista.Consola/Program.cs
@@ -1,5 +1,5 @@
 ﻿namespace Ista.Consola {
-    class Persona {
+    public abstract class Persona {
         private string nombre;
 
         public int Id { get; set; }
@@ -13,13 +13,44 @@
             } 
         }
         public bool EsConflictivo { get; }
+
+        public virtual void Dime() {
+            Console.WriteLine("Soy el padre");
+            
+        }
+        public void Add() {
+            Console.WriteLine("Propio del Padre");
+        }
+
+        public abstract void DebeSobreescribir();
+
+    }
+
+    public class Alumno: Persona {
+        public override void Dime() {
+            Console.WriteLine("Soy el Hijo");
+        }
+        public void Add() {
+            Console.WriteLine("Propio del Hijo");
+        }
+        public void Add(String algo) {
+            Console.WriteLine($"Propio del {algo}");
+        }
+
+        public override void DebeSobreescribir() {
+            throw new NotImplementedException();
+        }
     }
     internal class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
-            var p = new Persona();
+            Persona p = new Alumno();
             p.Nombre = "algo";
             Console.WriteLine(p.Nombre);
+            (p as Alumno).Add();
+            p.Dime();
+            Console.WriteLine(p.Nombre);
+            //p.Add();
         }
     }
 }
