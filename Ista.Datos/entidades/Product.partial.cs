91049b0b8017413625c79b624a81b1e927b6d2c7
Javier <jmartin@grupoloyal.com>
2024-05-28 12:34:32 +0200

    EF

---
+++ Ista.Datos/entidades/Product.partial.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ista.Dominio.Entidades;
+
+public partial class Product {
+    public void Descatalogar() {
+
+    }
+
+    public override bool Equals(object? obj) {
+        return obj is Product product &&
+               ProductId == product.ProductId;
+    }
+
+    public override int GetHashCode() {
+        return HashCode.Combine(ProductId);
+    }
+}
