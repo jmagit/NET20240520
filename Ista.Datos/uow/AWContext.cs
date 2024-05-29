f8f7a022c60ac18be3d6dd562d66c2dfab6082b4
Javier <jmartin@grupoloyal.com>
2024-05-29 09:36:31 +0200

    dbContext

--- Ista.Datos/uow/AWContext.cs
+++ Ista.Datos/uow/AWContext.cs
@@ -2,6 +2,7 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Ista.Dominio.Entidades;
 using Microsoft.EntityFrameworkCore;
 
@@ -40,7 +41,8 @@ public partial class AWContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=AdventureWorksLT2019;Trust Server Certificate=True;Persist Security Info=True;User ID=sa;Password=P@$$w0rd");
+        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=AdventureWorksLT2019;Trust Server Certificate=True;Persist Security Info=True;User ID=sa;Password=P@$$w0rd")
+        .LogTo(msg => Debug.WriteLine(msg));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
